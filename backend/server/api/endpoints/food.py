ab9a2115b57e0cbc2fbe70e41538c9543f526a29
Yuri Henrique <yurihcs@al.insper.edu.br>
2025-04-27 02:23:18 -0300

    modularizei a API externa e deixei mais genérico

---
+++ backend/server/api/endpoints/food.py
@@ -0,0 +1,16 @@
+import requests
+from flask import Blueprint, request, jsonify
+from server.api.db.database import db
+from server.api.external_api.fatsecret import search_food
+food_bp = Blueprint("food", __name__)
+
+@food_bp.route("/food/search", methods=["GET"])
+def search_food_route():
+    food_name = request.args.get("nome")
+    if not food_name:
+        return jsonify({"error": "Query param 'nome' is required"}), 400
+    try:
+        result = search_food(food_name)
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
---
+++ backend/server/api/external_api/fatsecret.py
@@ -0,0 +1,36 @@
+from server.api.core.config import Config
+import requests
+
+def get_access_token():
+    """Obtém o token de acesso OAuth 2.0 da FatSecret."""
+    url = "https://oauth.fatsecret.com/connect/token"
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {
+        "grant_type": "client_credentials",
+        "scope": "basic",
+        "client_id": Config.CLIENT_ID,
+        "client_secret": Config.CLIENT_SECRET
+    }
+    response = requests.post(url, headers=headers, data=data)
+    response.raise_for_status()
+    return response.json()["access_token"]
+
+def search_food(food_name):
+    """Pesquisa alimentos na FatSecret."""
+    token = get_access_token()
+    search_url = "https://platform.fatsecret.com/rest/server.api"
+
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {
+        "method": "foods.search",
+        "search_expression": food_name,
+        "format": "json"  # Sempre peça o retorno em JSON
+    }
+    response = requests.post(search_url, headers=headers, data=data)
+    response.raise_for_status()
+    return response.json()
