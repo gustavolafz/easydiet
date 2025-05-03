15fcc8ddbe7642dd97b45caa0da51a82d7c096a4
gustavolafz <gustavolaf@al.insper.edu.br>
2025-05-02 22:14:39 -0300

    implementa server/api/endpoints/user.py
    Description: User API endpoints for managing user data.

--- backend/app.py
+++ backend/app.py
@@ -1,31 +1,31 @@
 # app.py
 # Description: This is the main entry point for the Flask application. It initializes the app, registers blueprints, and sets up error handling.
+# Descrição: Este é o ponto de entrada principal para a aplicação Flask. Ele inicializa o app, registra blueprints e configura o tratamento de erros.
 
-import requests
+from flask import Flask
 from flask_cors import CORS
-from flask import Flask, jsonify
 from server.core.config import Config
 from server.api.endpoints.auth import auth_bp
 from server.api.endpoints.food import food_bp
 from server.core.error_handlers import register_error_handlers
 
-def create_app():
-    # Cria a aplicação Flask
+def create_app() -> Flask:
+    """
+    Cria e configura a instância da aplicação Flask.
+    """
     app = Flask(__name__)
-    
-    CORS(app)  # Habilita CORS para a aplicação
-    
-    # Configura as configurações globais (ex: chave secreta, debug, etc.)
+    CORS(app)
     app.config.from_object(Config)
-    
-    register_error_handlers(app)  # Registra os manipuladores de erro
+    register_error_handlers(app)
 
-    # Registra os blueprints
+    # Registra rotas da aplicação usando Blueprints
     app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(auth_bp, url_prefix='/user')
     app.register_blueprint(food_bp, url_prefix='')
 
     return app
 
+# Inicializa a aplicação
 app = create_app()
 
 if __name__ == "__main__":
